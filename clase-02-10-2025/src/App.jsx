b2c600111bd701d73f225531b167a229ac9e5966
csandobal <celestesandobal110@gmail.com>
2025-10-02 09:03:06 -0300

    todolist

--- clase-02-10-2025/src/App.jsx
+++ clase-02-10-2025/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Counter from './components/Counter'; 
-// Importar el archivo CSS con los estilos provistos
+import TodoList from './components/TodoList';
+
 import './styles/ComponentesHook.css'; 
 
 function App() {
   return (
     <div className="App">
+      <h2>Contador con límites</h2>
       <Counter />
+
+      <h2>Lista de Tareas Interactiva</h2>
+      <TodoList />
     </div>
   );
 }
-
 export default App;
---
+++ clase-02-10-2025/src/components/TodoList.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+function TodoList() {
+  // lógica de tareas...
+  return (
+    <div className="todo-list">
+      {/* input, lista y contador de pendientes */}
+    </div>
+  );
+}
+
+export default TodoList;
